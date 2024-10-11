366fb450eac25b506bc0a1043630b48801f1c13b
Andrea Mauriello <andrea.mauriello@socialthingum.com>
2024-10-11 09:32:42 +0200

    chore: setup Vite and Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Chat from "./components/Chat";
+
+const App = () => {
+  return;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      "aim-purple-1": "#f4efff",
+      "aim-purple-2": "#ded0ff",
+      "aim-purple-3": "#824cff",
+      "aim-purple-4": "#6834e2",
+      "aim-purple-5": "#2d1663",
+
+      "aim-white-1": "#ffffff",
+      "aim-white-2": "#f2f2f2",
+    },
+  },
+  plugins: [],
+};
